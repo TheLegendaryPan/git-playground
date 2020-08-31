7a7daa767d88342443300411ce269d3539387491
DevOps-Feng <panyongfeng1@gmail.com>
2020-08-31 23:46:05 +0100

    updated board_id for selenium test to use init file

--- Test/selenium_test.py
+++ Test/selenium_test.py
@@ -7,13 +7,15 @@ from threading import Thread
 import app
 from trelloapp import Trello
 from dotenv import load_dotenv
+from Test import board
+import json
 
 #file_path = find_dotenv('.env.test')
 #load_dotenv(file_path, override=True)
-username = os.getenv('username1')
-password = os.getenv('password')
-url = os.getenv('url')
-myboard = os.getenv('myboard')
+#username = os.getenv('username1')
+#password = os.getenv('password')
+#url = os.getenv('url')
+#myboard = os.getenv('myboard')
 
 # Module scope re-uses the fixture; note the webdriver chromedriver.exe is stored under python script folder
 @pytest.fixture(scope='module')
@@ -25,11 +27,10 @@ def driver():
 
 @pytest.fixture(scope='module')
 def test_app():
-    # Create the new board & update the board id environment variable
-    board_id = Trello().create_test_board("Tasks_Selenium")
-    #First of all, you're setting TRELLO_BOARD_ID, but TrelloApp.py is doing board_id= os.getenv("board_id")
-    #os.environ['TRELLO_BOARD_ID'] = board_id 
-    Trello.board_id = board_id
+    # new board created by calling trello from init file under Test
+    #board_id = Trello().create_test_board("Tasks_Selenium")
+    new_board = json.loads(board)
+    board_id = new_board["id"]
     # construct the new application
     application = app.create_app()
     # start the app in its own thread.
@@ -43,6 +44,7 @@ def test_app():
 
 def test_task_journey(driver, test_app):
     driver.get('http://localhost:5000/')
+    print(board_id)
     assert driver.current_url == 'http://localhost:5000/items/get_all_cards'
     assert driver.title == 'To-Do App'
 
@@ -60,27 +62,3 @@ def test_add_item_todo(driver, test_app):
     new_card = driver.find_element_by_css_selector("input[value=\"NewItem for Selenium\"]")
     assert new_card.get_attribute('value') == 'NewItem for Selenium'
     assert driver.current_url == 'http://localhost:5000/items/get_all_cards'
-
-
-
-#def test_trello(driver):
-#    driver.get(url)
-#    time.sleep(2)
-#    driver.find_element_by_id('user').send_keys(username)
-#    time.sleep(2)
-#
-#    loginclick = driver.find_elements_by_id('login')
-#    for loginclick in loginclick: 
-#        loginclick.send_keys(Keys.ENTER)
-#    time.sleep(2)
-
-#    loginpass = driver.find_element_by_id('password').send_keys(password)
-#    time.sleep(2)
-
-#    login4real = driver.find_element_by_id('login-submit').send_keys(Keys.ENTER)
-#    time.sleep(12)
-#
-#    driver.get(myboard)
-#    time.sleep(5)
-#    assert driver.current_url == myboard
-    
