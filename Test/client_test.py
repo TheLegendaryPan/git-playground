c2915a420b1f37e11853b53cf813d91fe7fc21ed
DevOps-Feng <panyongfeng1@gmail.com>
2021-02-18 23:39:12 +0000

    omg

--- Test/client_test.py
+++ Test/client_test.py
@@ -11,13 +11,12 @@ from unittest.mock import patch, Mock
 from dotenv import load_dotenv  #to invoke .env file
 import os
 
-load_dotenv()
-
 @pytest.fixture
 def client():
     # Use our test integration config instead of the 'real' version
-    file_path = find_dotenv('.env.test')
-    load_dotenv(file_path, override=True)
+    # file_path = find_dotenv('.env.test') removed post MONGO connection
+    # load_dotenv(file_path, override=True) removed post MONGO connection
+    load_dotenv()
     # Create the new app.
     test_app = app.create_app()
     # Use the app to create a test_client that can be used in our tests.
