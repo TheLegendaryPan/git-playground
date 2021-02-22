08439aab0b7ef09579775153cfc580e61a04345f
DevOps-Feng <panyongfeng1@gmail.com>
2021-02-22 23:22:35 +0000

    updated tests

--- Test/__init__.py
+++ Test/__init__.py
@@ -1,4 +1,4 @@
 from trelloapp import Trello
 
 # removed due to no need for selenium test
-# board = Trello().create_test_board("Tasks_Selenium")
+board = Trello().create_test_board("Tasks_Selenium")
